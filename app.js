b7bd1d0dd6c0c42d4e166a56743115d6ad61d474
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2023-10-10 13:19:15 +0500

    Completed

---
+++ app.js
@@ -0,0 +1,61 @@
+
+// select all buttons from keyboard div 
+const buttons = document.querySelectorAll(".keyboard button")
+
+// input  
+const resultInput = document.getElementById("resultInput")
+
+
+let value ="0"
+resultInput.value = 0;
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", e => {
+
+
+        if (e.target.innerText == "AC") {
+            value = 0
+            resultInput.value = value
+        }
+
+        else if (e.target.innerText == "DEL") {
+            value = value.substring(0, value.length - 1)
+            if (value.length < 1) {
+                value = 0;
+            }
+
+            resultInput.value = value
+        }
+
+
+
+
+        else if (e.target.innerText == "=") {
+            if (value.length < 1) {
+                value = 0
+            }
+            else {
+                value = eval(value)    
+            }
+
+            resultInput.value = value
+        }
+
+
+
+
+        else {
+            value += e.target.innerText
+            resultInput.value = value
+        }
+
+
+
+    })
+
+})
+
+
+// creatd by Muhammad Hasnain Ghazna 
+// github: mhgkhan // https://github.com/mhgkhan
